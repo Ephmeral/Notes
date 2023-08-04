048e59b0aef408edeb79e275a91137725b64faf8
Ephmeral <2669128599@qq.com>
2023-08-04 13:03:21 +0800

    Add: new C++ note, for =default & =delete

---
+++ cpp/default-delete/delete_new.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+struct type {
+  void *operator new(std::size_t) = delete;
+};
+
+type global_var;
+int main() {
+  static type static_var;
+  type auto_var;
+  type *var_ptr = new type;  // 编译失败，该类的new已被删除
+}
